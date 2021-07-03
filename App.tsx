fb897c9c6e9921123a48a577796479a642697550
Maria F <mariadefatima775@hotmail.com>
2021-07-03 08:30:55 -0300

    auth

--- App.tsx
+++ App.tsx
@@ -14,6 +14,8 @@ import {
 
 import theme from './src/global/styles/theme';
 import {AppRoutes} from './src/routes/app.routes';
+import SignIn from './src/screens/SignIn';
+import { AuthProvider, useAuth } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +32,10 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer >
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <AuthProvider>
+          {/* <AppRoutes /> */}
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
